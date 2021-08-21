2c8fc2f305d5e92bc6379f27b3060fb545fa4c33
AntoineCharette <antoineocharette@gmail.com>
2021-08-20 20:25:13 -0400

    adds the styling for the admin page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import { Component } from 'react'
-import Header from './components/header'
-import CreateForm from './components/create-form'
-import ReportTable from './components/report-table'
-import Footer from './components/footer'
-import { hours } from './data'
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
+import CreateForm from './components/create-form';
+import ReportTable from './components/report-table';
+import Footer from './components/footer';
+import { hours } from './data';
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      standReports: []
-    }
+      standReports: [],
+    };
   }
 
   handleCreate = (standData) => {
     const standReports = [...this.state.standReports, standData];
     this.setState({ standReports });
-  }
+  };
 
   render() {
     return (
@@ -28,7 +28,7 @@ class App extends Component {
           <ReportTable hours={hours} reports={this.state.standReports} />
         </main>
         <Footer reports={this.state.standReports} />
-      </div >
+      </div>
     );
   }
 }
--- src/components/create-form.js
+++ src/components/create-form.js
@@ -1,43 +1,53 @@
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FormGroup from 'react-bootstrap/FormGroup';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 export default function CreateForm({ onCreate }) {
+  function submitHandler(event) {
+    event.preventDefault();
+    onCreate({
+      id: event.target.location.value,
+      location: event.target.location.value,
+      hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36],
+    });
+    event.target.reset();
+  }
+  return (
+    <Container style={{ backgroundColor: '#C9B2D9', padding: '20px', borderRadius: '10px', width: '600px' }}>
+      <Form onSubmit={submitHandler}>
+        <legend style={{ color: 'black', fontSize: '2rem', textAlign: 'center' }}>Create Cookie Stand</legend>
+        <Row>
+          <Col>
+            <FormGroup style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <Form.Label htmlFor="location">Location</Form.Label>
+              <Form.Control style={{ width: '200px' }} type="text" name="location" />
+            </FormGroup>
+            <Button style={{ marginTop: '110px', marginBottom: '10px' }} type="submit">
+              Create
+            </Button>
+          </Col>
 
-    function submitHandler(event) {
-        event.preventDefault();
-        onCreate({
-            id: event.target.location.value,
-            location: event.target.location.value,
-            hourly_sales: [48, 42, 30, 24, 42, 24, 36, 42, 42, 48, 36, 42, 24, 36]
-        });
-        event.target.reset();
-    }
-    return (
-        <form onSubmit={submitHandler}>
+          <Col>
+            <FormGroup style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <Form.Label htmlFor="min-customers">Minimum Customers per Hour</Form.Label>
+              <Form.Control style={{ width: '200px' }} type="number" name="min-customers" />
+            </FormGroup>
 
-            <legend>Create Cookie Stand</legend>
+            <FormGroup style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <Form.Label htmlFor="max-customers">Maximum Customers per Hour</Form.Label>
+              <Form.Control style={{ width: '200px' }} type="number" name="max-customers" />
+            </FormGroup>
 
-            <div>
-                <label htmlFor="location">Location</label>
-                <input type="text" name="location" />
-            </div>
-
-            <div>
-
-                <fieldset>
-                    <label htmlFor="min-customers">Minimum Customers per Hour</label>
-                    <input type="number" name="min-customers" />
-                </fieldset>
-
-                <fieldset>
-                    <label htmlFor="max-customers">Maximum Customers per Hour</label>
-                    <input type="number" name="max-customers" />
-                </fieldset>
-
-                <fieldset>
-                    <label htmlFor="avg-cookies">Average Cookies per Sale</label>
-                    <input type="number" name="avg-cookies" step=".1" />
-                </fieldset>
-                <button type="submit">Create</button>
-            </div>
-
-        </form>
-    )
+            <FormGroup style={{ marginTop: '10px', marginBottom: '10px' }}>
+              <Form.Label htmlFor="avg-cookies">Average Cookies per Sale</Form.Label>
+              <Form.Control style={{ width: '200px' }} type="number" name="avg-cookies" step=".1" />
+            </FormGroup>
+          </Col>
+        </Row>
+      </Form>
+    </Container>
+  );
 }
